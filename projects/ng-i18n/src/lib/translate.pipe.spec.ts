fac7bb3ab7f7b076553a08859ba56700a925ddda
Marco Buschini <marco.buschini@gmail.com>
2020-03-21 21:39:02 +0100

    feat(pipe): implement the translation pipe

--- projects/ng-i18n/src/lib/translate.pipe.spec.ts
+++ projects/ng-i18n/src/lib/translate.pipe.spec.ts
@@ -1,8 +1,58 @@
 import { TranslatePipe } from './translate.pipe'
+import { TestBed } from '@angular/core/testing'
+import { TranslateService } from './translate.service'
+import { Culture } from './culture'
+import { Translations } from './translations'
+import { of, ReplaySubject } from 'rxjs'
+import { AsyncPipe } from '@angular/common'
+import { ChangeDetectorRef } from '@angular/core'
 
 describe('TranslatePipe', () => {
+  let pipe: TranslatePipe
+  let service: TranslateService
+  let async: AsyncPipe
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [AsyncPipe, TranslateService, ChangeDetectorRef],
+    })
+    service = TestBed.inject(TranslateService)
+    async = TestBed.inject(AsyncPipe)
+    pipe = new TranslatePipe(service, async)
+  })
+
   it('should create an instance', () => {
-    const pipe = new TranslatePipe()
     expect(pipe).toBeTruthy()
+  })
+
+  it('should load translations and translate a string', () => {
+    const english = new Map<string, string>()
+    english.set(
+      'TEST_TRANSLATION',
+      'There are two parameters: {KEY_1} and {KEY_2}'
+    )
+
+    const americanCulture = {
+      isoCode: 'en_US',
+      name: 'English (United States)',
+    } as Culture
+    const translations = {
+      culture: americanCulture,
+      translations: of(english),
+    } as Translations
+    service.addCulture(americanCulture, translations)
+    service.setCulture(americanCulture)
+
+    const params = new Map<string, string>()
+    params.set('KEY_1', 'First parameter')
+    params.set('KEY_2', 'Second parameter')
+    const paramSubject = new ReplaySubject<Map<string, string>>(1)
+    paramSubject.next(params)
+    expect(
+      pipe.transform('TEST_TRANSLATION', paramSubject.asObservable())
+    ).toEqual<string>(
+      'There are two parameters: {KEY_1} and {KEY_2} [KEY_1: First parameter, KEY_2: Second parameter, ]'
+    )
+    service.removeCulture(americanCulture)
   })
 })
--- projects/ng-i18n/src/lib/translate.pipe.ts
+++ projects/ng-i18n/src/lib/translate.pipe.ts
@@ -1,10 +1,27 @@
-import { Pipe, PipeTransform } from '@angular/core'
+import { Pipe, PipeTransform, ChangeDetectorRef } from '@angular/core'
+import { TranslateService } from './translate.service'
+import { Observable } from 'rxjs'
+import { AsyncPipe } from '@angular/common'
 
+/**
+ * Asynchronous pipe to translate a string with parameters.
+ */
 @Pipe({
   name: 'translate',
 })
 export class TranslatePipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null
+  constructor(
+    private service: TranslateService,
+    private asyncPipe: AsyncPipe
+  ) {}
+
+  /**
+   * Actually applies the pipe generating a translated string with parameters.
+   *
+   * @param value the translation key
+   * @param args the translation key parameters map
+   */
+  transform(value: string, args: Observable<Map<string, string>>): string {
+    return this.asyncPipe.transform(this.service.translate(value, args))
   }
 }
