78e30aaa7bca60018b2a8f122d61a6c69695b2ff
Marco Buschini <marco.buschini@gmail.com>
2020-04-24 13:48:41 +0200

    fix(package): export Culture and Translations that wasn't previously exported

--- projects/ng-i18n/src/public-api.ts
+++ projects/ng-i18n/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/ng-i18n.module'
 export * from './lib/translate.pipe'
 export * from './lib/translate.service'
+export * from './lib/culture'
+export * from './lib/translations'
