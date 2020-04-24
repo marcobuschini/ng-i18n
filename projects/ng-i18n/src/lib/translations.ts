eba84aba84089bb49e1089136e3b0849347bebba
Marco Buschini <marco.buschini@gmail.com>
2020-04-24 15:42:14 +0200

    fix(translations.ts): remove the culture property from the Translations class
    This was causing duplicated entries as the same value is used as the key in the translations array.
    
    BREAKING CHANGE: The culture property is no longer available in the Translations class.

--- projects/ng-i18n/src/lib/translations.ts
+++ projects/ng-i18n/src/lib/translations.ts
@@ -1,12 +1,9 @@
-import { Culture } from './culture'
 import { Observable } from 'rxjs'
 
 /**
  * Represents a set of translation strings for a given culture.
  */
 export class Translations {
-  /** The culture these translation strings refer to. */
-  public culture: Culture
   /** The translation strings for the culture. */
   public translations: Observable<
     Map<string, (d: Map<string, string>) => string>
